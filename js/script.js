3936db75232a5436aac40292f441f3348aaabd3e
Ochibo-JJ <133512040+Ochibo-JJ@users.noreply.github.com>
2023-06-07 00:05:39 +0300

    started writing code for the rocket

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+		if(currentClassName== 'cool') {
+			document.getElementById ('cool').className = 'cool red';
+		} else {
+			document.getElementById ('cool').className = 'cool';
+		}
+	//document.getElementById ('cool').className = 'cool red';
+}
+/*var SayMyName = function (name) {
+	alert('My name is: '+name);
+}
+var car = {
+			make: 'VW',
+			type: 'Polo',
+			color: 'blue',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: ['seat1','seat2','seat3','seat4'],
+			turnOn: function () {
+				this.isTurnedOn = true;
+			},
+			fly: function () {
+				alert('fly');
+			}
+		};
+		console.log('Hello there');*/
