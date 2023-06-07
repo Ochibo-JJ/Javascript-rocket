a6345a1b6a99b540ea16f2267ffdc185dc95fb5f
Ochibo-JJ <133512040+Ochibo-JJ@users.noreply.github.com>
2023-06-07 23:54:37 +0300

    wrote js for rocket

--- js/script.js
+++ js/script.js
@@ -1,4 +1,62 @@
-var doCoolStuff = function () {
+var timer = null;
+var countdownNumber = 10;
+var changeState = function (state){
+	document.body.className = 'body-state' + state;
+	clearInterval(timer);
+	countdownNumber = 10;
+	document.getElementById ('countdown').innerHTML = countdownNumber;
+
+	//countdown
+	if (state==2) {
+		timer = setInterval(function () {
+			countdownNumber = countdownNumber-1;
+			document.getElementById ('countdown').innerHTML = countdownNumber;
+			if (countdownNumber<=-1) {				
+				changeState(3);
+			}
+		},500);
+		
+	}else if (state==3) {
+		var success = setTimeout(function ()
+			{
+				var randomNumber = Math.round(Math.random()*10);
+				console.log('randomNumber',randomNumber)
+				//success
+				if (randomNumber>1) {
+					changeState(4);
+				//failure
+				}else {
+					changeState(5);
+				}
+			}, 2000);
+	};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var doCoolStuff = function () {
 	var currentClassName = document.getElementById('cool').className;
 		if(currentClassName== 'cool') {
 			document.getElementById ('cool').className = 'cool red';
@@ -7,7 +65,7 @@ var doCoolStuff = function () {
 		}
 	//document.getElementById ('cool').className = 'cool red';
 }
-/*var SayMyName = function (name) {
+var SayMyName = function (name) {
 	alert('My name is: '+name);
 }
 var car = {
